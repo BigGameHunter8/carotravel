2425a57763cc8f92b99c779df3a209fef5074755
Dima <admin@Admins-iMac-2.local>
2018-03-22 17:50:45 +0200

    slider plus first part of second

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
-$(document).ready(function(){
-    
-});
+var img= document.getElementById("img");
+var par= document.getElementById("par");
+
+console.log(img.style.zIndex);
+console.log(par.style.zIndex);
